becf8b6bb8903862e64ef4ae11a61e6a7c7764b9
Rick <rct999@gmail.com>
2024-01-17 05:09:22 +0100

    init

---
+++ fm_init_janome.py
@@ -0,0 +1,16 @@
+from typing import Callable
+from janome.tokenizer import Tokenizer
+
+JANOME_TOKENIZER = Tokenizer()
+
+def janome_tokenizer_provided_via_user_file_init(txt: str) -> list[str]:
+
+    token_list = list(token for token in JANOME_TOKENIZER.tokenize(txt, wakati=True) if isinstance(token, str))
+    return token_list
+
+
+def janome_tokenizers_provider() -> list[tuple[str, Callable]]:
+
+    return [("ja", janome_tokenizer_provided_via_user_file_init)]
+
+
