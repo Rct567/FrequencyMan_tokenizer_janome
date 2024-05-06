e1546be8c17464585403cd428907a379b623b8a1
Rick Zuidhoek <rct999@gmail.com>
2024-05-07 00:14:21 +0200

    Update fm_init_janome.py

--- fm_init_janome.py
+++ fm_init_janome.py
@@ -3,7 +3,7 @@ from janome.tokenizer import Tokenizer
 
 JANOME_TOKENIZER = Tokenizer()
 
-def janome_tokenizer_provided_via_user_file_init(txt: str) -> list[str]:
+def janome_tokenizer_provided_via_user_files_init(txt: str) -> list[str]:
 
     token_list = list(token for token in JANOME_TOKENIZER.tokenize(txt, wakati=True) if isinstance(token, str))
     return token_list
